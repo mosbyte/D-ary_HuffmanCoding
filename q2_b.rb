832afe9ebed99ef3048345de8de9898563f451cf
Tomás Mc Phillips <tomas.mc-phillips@ucdconnect.ie>
2018-05-10 17:03:45 +0100

    writes to files

--- q2_b.rb
+++ q2_b.rb
@@ -3,7 +3,7 @@ require_relative 'sequence_generator'
 class Q2B
 
 
-  n = 10000
+  n = 1000000
   probabilities = [0.2141, 0.2648, 0.3207, 0.2004]
   symbols = %w(A T C G)
   q2 = SequenceGenerator.new
